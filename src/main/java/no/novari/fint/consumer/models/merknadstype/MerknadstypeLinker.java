c473ecd5c8aaa9834784dac5e18d1ca1884dc61b
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2026-01-15 15:28:01 +0100

    Version 4.0.0-rc-6.

---
+++ src/main/java/no/novari/fint/consumer/models/merknadstype/MerknadstypeLinker.java
@@ -0,0 +1,64 @@
+package no.novari.fint.consumer.models.merknadstype;
+
+import no.novari.fint.model.resource.arkiv.kodeverk.MerknadstypeResource;
+import no.novari.fint.model.resource.arkiv.kodeverk.MerknadstypeResources;
+import no.novari.fint.relations.FintLinker;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.Objects.isNull;
+import static org.springframework.util.StringUtils.isEmpty;
+
+@Component
+public class MerknadstypeLinker extends FintLinker<MerknadstypeResource> {
+
+    public MerknadstypeLinker() {
+        super(MerknadstypeResource.class);
+    }
+
+    public void mapLinks(MerknadstypeResource resource) {
+        super.mapLinks(resource);
+    }
+
+    @Override
+    public MerknadstypeResources toResources(Collection<MerknadstypeResource> collection) {
+        return toResources(collection.stream(), 0, 0, collection.size());
+    }
+
+    @Override
+    public MerknadstypeResources toResources(Stream<MerknadstypeResource> stream, int offset, int size, int totalItems) {
+        MerknadstypeResources resources = new MerknadstypeResources();
+        stream.map(this::toResource).forEach(resources::addResource);
+        addPagination(resources, offset, size, totalItems);
+        return resources;
+    }
+
+    @Override
+    public String getSelfHref(MerknadstypeResource merknadstype) {
+        return getAllSelfHrefs(merknadstype).findFirst().orElse(null);
+    }
+
+    @Override
+    public Stream<String> getAllSelfHrefs(MerknadstypeResource merknadstype) {
+        Stream.Builder<String> builder = Stream.builder();
+        if (!isNull(merknadstype.getSystemId()) && !isEmpty(merknadstype.getSystemId().getIdentifikatorverdi())) {
+            builder.add(createHrefWithId(merknadstype.getSystemId().getIdentifikatorverdi(), "systemid"));
+        }
+        
+        return builder.build();
+    }
+
+    int[] hashCodes(MerknadstypeResource merknadstype) {
+        IntStream.Builder builder = IntStream.builder();
+        if (!isNull(merknadstype.getSystemId()) && !isEmpty(merknadstype.getSystemId().getIdentifikatorverdi())) {
+            builder.add(merknadstype.getSystemId().getIdentifikatorverdi().hashCode());
+        }
+        
+        return builder.build().toArray();
+    }
+
+}
+
---
+++ src/main/java/no/novari/fint/consumer/models/partrolle/PartRolleCacheService.java
@@ -0,0 +1,135 @@
+package no.novari.fint.consumer.models.partrolle;
+
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import lombok.extern.slf4j.Slf4j;
+
+import no.fint.cache.CacheService;
+import no.fint.cache.model.CacheObject;
+import no.novari.fint.consumer.config.Constants;
+import no.novari.fint.consumer.config.ConsumerProps;
+import no.novari.fint.consumer.event.ConsumerEventUtil;
+import no.fint.event.model.Event;
+import no.fint.event.model.ResponseStatus;
+import no.novari.fint.relations.FintResourceCompatibility;
+import no.novari.fint.model.resource.FintLinks;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import no.novari.fint.model.arkiv.kodeverk.PartRolle;
+import no.novari.fint.model.resource.arkiv.kodeverk.PartRolleResource;
+import no.novari.fint.model.arkiv.kodeverk.KodeverkActions;
+import no.novari.fint.model.felles.kompleksedatatyper.Identifikator;
+
+@Slf4j
+@Service
+@ConditionalOnProperty(name = "fint.consumer.cache.disabled.partrolle", havingValue = "false", matchIfMissing = true)
+public class PartRolleCacheService extends CacheService<PartRolleResource> {
+
+    public static final String MODEL = PartRolle.class.getSimpleName().toLowerCase();
+
+    @Value("${fint.consumer.compatibility.fintresource:true}")
+    private boolean checkFintResourceCompatibility;
+
+    @Autowired
+    private FintResourceCompatibility fintResourceCompatibility;
+
+    @Autowired
+    private ConsumerEventUtil consumerEventUtil;
+
+    @Autowired
+    private ConsumerProps props;
+
+    @Autowired
+    private PartRolleLinker linker;
+
+    private JavaType javaType;
+
+    private ObjectMapper objectMapper;
+
+    public PartRolleCacheService() {
+        super(MODEL, KodeverkActions.GET_ALL_PARTROLLE, KodeverkActions.UPDATE_PARTROLLE);
+        objectMapper = new ObjectMapper();
+        javaType = objectMapper.getTypeFactory().constructCollectionType(List.class, PartRolleResource.class);
+        objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+    }
+
+    @PostConstruct
+    public void init() {
+        props.getAssets().forEach(this::createCache);
+    }
+
+    @Scheduled(initialDelayString = Constants.CACHE_INITIALDELAY_PARTROLLE, fixedRateString = Constants.CACHE_FIXEDRATE_PARTROLLE)
+    public void populateCacheAll() {
+        props.getAssets().forEach(this::populateCache);
+    }
+
+    public void rebuildCache(String orgId) {
+		flush(orgId);
+		populateCache(orgId);
+	}
+
+    @Override
+    public void populateCache(String orgId) {
+		log.info("Populating PartRolle cache for {}", orgId);
+        Event event = new Event(orgId, Constants.COMPONENT, KodeverkActions.GET_ALL_PARTROLLE, Constants.CACHE_SERVICE);
+        consumerEventUtil.send(event);
+    }
+
+
+    public Optional<PartRolleResource> getPartRolleBySystemId(String orgId, String systemId) {
+        return getOne(orgId, systemId.hashCode(),
+            (resource) -> Optional
+                .ofNullable(resource)
+                .map(PartRolleResource::getSystemId)
+                .map(Identifikator::getIdentifikatorverdi)
+                .map(systemId::equals)
+                .orElse(false));
+    }
+
+
+	@Override
+    public void onAction(Event event) {
+        List<PartRolleResource> data;
+        if (checkFintResourceCompatibility && fintResourceCompatibility.isFintResourceData(event.getData())) {
+            log.info("Compatibility: Converting FintResource<PartRolleResource> to PartRolleResource ...");
+            data = fintResourceCompatibility.convertResourceData(event.getData(), PartRolleResource.class);
+        } else {
+            data = objectMapper.convertValue(event.getData(), javaType);
+        }
+        data.forEach(resource -> {
+            linker.mapLinks(resource);
+            linker.resetSelfLinks(resource);
+        });
+        if (KodeverkActions.valueOf(event.getAction()) == KodeverkActions.UPDATE_PARTROLLE) {
+            if (event.getResponseStatus() == ResponseStatus.ACCEPTED || event.getResponseStatus() == ResponseStatus.CONFLICT) {
+                List<CacheObject<PartRolleResource>> cacheObjects = data
+                    .stream()
+                    .map(i -> new CacheObject<>(i, linker.hashCodes(i)))
+                    .collect(Collectors.toList());
+                addCache(event.getOrgId(), cacheObjects);
+                log.info("Added {} cache objects to cache for {}", cacheObjects.size(), event.getOrgId());
+            } else {
+                log.debug("Ignoring payload for {} with response status {}", event.getOrgId(), event.getResponseStatus());
+            }
+        } else {
+            List<CacheObject<PartRolleResource>> cacheObjects = data
+                    .stream()
+                    .map(i -> new CacheObject<>(i, linker.hashCodes(i)))
+                    .collect(Collectors.toList());
+            updateCache(event.getOrgId(), cacheObjects);
+            log.info("Updated cache for {} with {} cache objects", event.getOrgId(), cacheObjects.size());
+        }
+    }
+}
